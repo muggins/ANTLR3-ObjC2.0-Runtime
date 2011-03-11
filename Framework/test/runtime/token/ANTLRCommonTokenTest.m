bff42c067f52df0814be5110fa964e4e0d40ca41
Alan Condit <acondit@ipns.com>
2011-03-11 08:02:07 -0800

    A bunch more changes related to work on ST4 implementation and some bugfixes resulting from that work.

--- Framework/test/runtime/token/ANTLRCommonTokenTest.m
+++ Framework/test/runtime/token/ANTLRCommonTokenTest.m
@@ -14,7 +14,7 @@
 
 -(void) testInitAndRelease
 {
-	ANTLRCommonToken *token = [ANTLRCommonToken newANTLRCommonToken];
+	ANTLRCommonToken *token = [ANTLRCommonToken newToken];
 	STAssertNotNil(token, @"Token was nil");
 	[token release];
 }
@@ -29,7 +29,7 @@
 
 -(void) testInitWithTokenType
 {
-	ANTLRCommonToken *token = [ANTLRCommonToken newANTLRCommonToken:ANTLRTokenTypeUP];
+	ANTLRCommonToken *token = [ANTLRCommonToken newToken:ANTLRTokenTypeUP];
 	token.text = @"<UP>";
 	STAssertNotNil(token, @"Token was nil");
 	STAssertEquals(token.type, (NSInteger)ANTLRTokenTypeUP, @"Token was not of type ANTLRTokenTypeUP");
@@ -40,7 +40,7 @@
 
 -(void) testInitWithTokenTypeAndText
 {
-	ANTLRCommonToken *token = [ANTLRCommonToken newANTLRCommonToken:ANTLRTokenTypeUP Text:@"<UP>"];
+	ANTLRCommonToken *token = [ANTLRCommonToken newToken:ANTLRTokenTypeUP Text:@"<UP>"];
 	STAssertNotNil(token, @"Token was nil");
 	STAssertEquals(token.type, (NSInteger)ANTLRTokenTypeUP, @"Token was not of type ANTLRTokenTypeUP");
 	STAssertNotNil(token.text, @"Token text was nil, was expecting <UP>");
@@ -51,7 +51,7 @@
 -(void) testInitWithCharStream
 {
 	ANTLRStringStream *stream = [ANTLRStringStream newANTLRStringStream:@"this||is||a||double||piped||separated||csv"];
-	ANTLRCommonToken *token = [ANTLRCommonToken newANTLRCommonToken:stream Type:555 Channel:ANTLRTokenChannelDefault Start:4 Stop:5];
+	ANTLRCommonToken *token = [ANTLRCommonToken newToken:stream Type:555 Channel:ANTLRTokenChannelDefault Start:4 Stop:5];
 	STAssertNotNil(token, @"Token was nil");
 	STAssertEquals(token.type, (NSInteger)555, @"Token was not of type 555"); // Nice random type number
 	STAssertNotNil(token.text, @"Token text was nil, was expecting ||");
@@ -62,13 +62,13 @@
 -(void) testInitWithToken
 {
 	ANTLRStringStream *stream = [ANTLRStringStream newANTLRStringStream:@"this||is||a||double||piped||separated||csv"];
-	ANTLRCommonToken *token = [ANTLRCommonToken newANTLRCommonToken:stream Type:555 Channel:ANTLRTokenChannelDefault Start:4 Stop:5];
+	ANTLRCommonToken *token = [ANTLRCommonToken newToken:stream Type:555 Channel:ANTLRTokenChannelDefault Start:4 Stop:5];
 	STAssertNotNil(token, @"Token was nil");
 	STAssertEquals(token.type, (NSInteger)555, @"Token was not of type 555"); // Nice random type number
 	STAssertNotNil(token.text, @"Token text was nil, was expecting ||");
 	STAssertTrue([token.text isEqualToString:@"||"], @"Token text was not || was instead: %@", token.text);
 	
-	ANTLRCommonToken *newToken = [ANTLRCommonToken newANTLRCommonTokenWithToken:token];
+	ANTLRCommonToken *newToken = [ANTLRCommonToken newTokenWithToken:token];
 	STAssertNotNil(newToken, @"New token is nil!");
 	STAssertEquals(newToken.type, token.type, @"Tokens types do not match %d:%d!", newToken.type, token.type);
 	STAssertEquals(newToken.line, token.line, @"Token lines do not match!");
@@ -86,7 +86,7 @@
 {
     NSString *aDescription;
 	ANTLRStringStream *stream = [ANTLRStringStream newANTLRStringStream:@"this||is||a||double||piped||separated||csv"];
-	ANTLRCommonToken *token = [ANTLRCommonToken newANTLRCommonToken:stream Type:555 Channel:ANTLRTokenChannelDefault Start:4 Stop:5];
+	ANTLRCommonToken *token = [ANTLRCommonToken newToken:stream Type:555 Channel:ANTLRTokenChannelDefault Start:4 Stop:5];
     aDescription = [token description];
 	STAssertTrue([aDescription isEqualToString:@"[@0, 4:5='||',<555>,0:0]"], @"String description for token is not correct! got %@", aDescription);
 }
